7dd67da5077af0db48f59fcf424b32741a272584
wiki_z <48912577+zhanghanting@users.noreply.github.com>
2019-06-04 21:03:30 +0800

    Delete IMtimeAreaDictTServiceImpl.java

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/modular/cinema/service/impl/IMtimeAreaDictTServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.stylefeng.guns.rest.modular.cinema.service.impl;
-
-import com.stylefeng.guns.rest.cinema.service.IMtimeAreaDictTService;
-
-public class IMtimeAreaDictTServiceImpl implements IMtimeAreaDictTService
-{
-
-}
